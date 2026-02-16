a0a8244eb24c79361b753adf7b895698b0f16a93
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-16 22:38:42 +0530

    pattern program 1

---
+++ pattern1.java
@@ -0,0 +1,10 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= i; i++) {
+            for( int j= 0; j<= j;j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+    }
+}

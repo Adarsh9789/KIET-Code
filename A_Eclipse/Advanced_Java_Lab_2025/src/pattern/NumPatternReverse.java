c5da6250b1555c4aba5ba0f17f349a79d0cc87e5
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-19 15:32:39 +0530

    Pattern Reverse

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/pattern/NumPatternReverse.java
@@ -0,0 +1,15 @@
+package pattern;
+
+public class NumPatternReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row = 11;
+		for(int i=row -1;i>=0;i--) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(i-j+" ");
+			}
+			System.out.println();
+		}
+	}
+}

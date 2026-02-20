4e088ec50162ea26b00145b642e0b857832b62f6
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-20 16:15:33 +0530

    palindrome string using lambda expression

--- A_Eclipse/Advanced_Java_Lab_2025/src/lamda_expression/PalindromeMain.java
+++ A_Eclipse/Advanced_Java_Lab_2025/src/lamda_expression/PalindromeMain.java
@@ -9,11 +9,28 @@ public class PalindromeMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CheckPalindrome cp = (String info)->{
-			String infoRev = new StringBuilder(info).reverse().toString();
-			return infoRev.equals(info);
-		};
-
+		CheckPalindrome cp =  info-> new StringBuilder(info).reverse().toString().equals(info);
+		
+		//CheckPalindrome cap = (String info)->new StringBuilder(info).reverse().toString().equals(info);
+        System.out.println(cp.checkPalin("Ayush"));  // false
+        System.out.println(cp.checkPalin("madam"));  // true
+	}
 	}
 
-}
+//
+//@FunctionalInterface
+//interface CheckPalindrome {
+//    boolean checkPalin(String info);
+//}
+//
+//public class PalindromeMain {
+//
+//    public static void main(String[] args) {
+//
+//        CheckPalindrome cp = info -> 
+//            new StringBuilder(info).reverse().toString().equals(info);
+//
+//        System.out.println(cp.checkPalin("Ayush"));  // false
+//        System.out.println(cp.checkPalin("madam"));  // true
+//    }
+//}

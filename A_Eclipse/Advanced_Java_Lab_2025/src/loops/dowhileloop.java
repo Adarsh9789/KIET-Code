635bd9c29fe157978e0ce6461b13619bc020d19d
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-21 22:29:27 +0530

    for and do while loop syntax practice

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/loops/dowhileloop.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class dowhileloop {
+	public static void main(String[] args) {
+		int i=0;
+		do{
+			System.out.println("I ki value"+i);
+			i++;
+		}
+		while(i<=5);
+		
+		int j=5;
+		do {
+			System.out.println("J ki value"+j);
+			j--;
+		}while(j>=0);
+	}
+
+}
---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/loops/forloop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0;i<=5;i++) {
+			System.out.println("I ki value"+i);
+		}
+		for(int j = 5;j>=0;j--) {
+			System.out.println("J ki value"+j);
+		}
+
+	}
+
+}

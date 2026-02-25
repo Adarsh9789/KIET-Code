23bca85ebc99e28eea41561b19956455ae5941ee
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-25 23:30:39 +0530

    Added Main Method 3rd Syntax

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/main_method/MainMethod3.java
@@ -0,0 +1,11 @@
+package main_method;
+
+public class MainMethod3 {
+
+	public static void main(String... args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello 3");
+
+	}
+
+}
---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/switch_case/MainYeild.java
@@ -0,0 +1,13 @@
+package switch_case;
+
+public class MainYeild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Abc ");
+		
+
+	}
+
+}

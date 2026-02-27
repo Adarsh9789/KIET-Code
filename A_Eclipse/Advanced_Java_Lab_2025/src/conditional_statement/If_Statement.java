a1add0afa1e7f49082478a41ac1a6ab73fe8b488
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-02-27 23:47:13 +0530

    Created If Statement Syntax Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/conditional_statement/If_Statement.java
@@ -0,0 +1,11 @@
+package conditional_statement;
+
+public class If_Statement {
+    public static void main(String[] args) {
+        int n = 10;
+
+        if (n > 0) {
+            System.out.println("Number is positive");
+        }
+    }
+}

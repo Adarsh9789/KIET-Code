f17a5f9efe7e521dd20d3e5bf8002ed07679ea0c
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-03 15:43:03 +0530

    Created Real Number to Practice Data Types

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/datatypes/RealNumber.java
@@ -0,0 +1,45 @@
+package datatypes;
+
+public class RealNumber {
+
+	public static void main(String... args) {
+		
+		//float a = 1.232;
+		float a = 1.43233223423423424232f;
+		float b = 2.1314433324233242f;
+		
+		float c = a+b;
+		
+		System.out.println(c);
+
+		float d = 32.43233223423423424232f;
+		float e = 2.1314433324233242f;
+		
+		float f = d+e;
+		
+		System.out.println(f);
+		
+		float g = 01231;
+		System.out.println(g);
+
+		float h = 1231;
+		System.out.println(h);
+		
+		double i = 2.23242423;
+		double j = 2.123124124124132;
+		
+		double k = i+j;
+		
+		System.out.println(k);
+
+		double l = 21.2324242365862424;
+		double m = 49.123124124124132;
+		
+		double n = l+m;
+		
+		System.out.println(n);
+
+		
+	}
+
+}

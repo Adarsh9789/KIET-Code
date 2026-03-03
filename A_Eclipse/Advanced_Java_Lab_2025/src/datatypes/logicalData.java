f751a6afcae371e2a3ea5ecf9de33049ce1864ed
Adarsh9789 <adarshkatiyar198@gmail.com>
2026-03-03 15:50:50 +0530

    Created Logical Data to Practice Data Types

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/datatypes/logicalData.java
@@ -0,0 +1,22 @@
+package datatypes;
+
+public class logicalData {
+	
+	public static void main(String[] args) {
+		
+		boolean a = true;
+		
+		System.out.println(a);
+		
+		boolean b = false;
+		
+		System.out.println(b);
+		
+		boolean c ;
+		System.out.println(c);
+		System.out.println(a);
+
+
+	}
+
+}

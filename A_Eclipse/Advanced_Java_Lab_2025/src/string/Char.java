be5e07c4bce6fb40e52d8f3938d31aaec02e9838
AdarshAlgo <adarshkatiyar198@gmail.com>
2026-06-05 00:00:49 +0530

    Created Character Printing Program

---
+++ A_Eclipse/Advanced_Java_Lab_2025/src/string/Char.java
@@ -0,0 +1,15 @@
+package string;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s= "abcd";
+		int i =4;
+		while(i>0) {
+		System.out.println(s.charAt(i-1));
+		i--;
+		}
+	}
+
+}
